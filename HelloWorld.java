0ebb101441eed291ce25f2846d54341bce6fa7a0
SuchitraHole <52686721+SuchitraHole@users.noreply.github.com>
2021-03-22 16:42:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld{
   public static void main(String [] args){
     System.out.print("hiii");
   }
-
+public static void add(int num1, int num2){
+    System.out.print(num1+num2);
+  }
 }
